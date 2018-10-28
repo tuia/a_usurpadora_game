a6a94b93809653c452d849cb394bc97de2681c6b
Nathália Xavier <nathaliazxavier@gmail.com>
2018-10-28 19:14:16 +0100

    Moved update stats message css out of js

--- js/main.js
+++ js/main.js
@@ -7,13 +7,7 @@
 
 function displayUpdateStats() {
     var element = $("<div>");
-    element.addClass("modal-test");
-    element.css("position", "absolute");
-    element.css("left", 50);
-    element.css("top", 50);
-    element.css("z-index", 99999);
-    element.css("font-size", 48);
-    element.css("color", "white");
+    element.addClass("update-stats");
     element.text("Updating stats...");
     $("body").append(element);
     setTimeout(function(){
