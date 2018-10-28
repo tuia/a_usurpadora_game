244683fee6f5fc49a8c2619eef4049ae1002dc1c
Nathália Xavier <nathaliazxavier@gmail.com>
2018-10-28 20:07:46 +0100

    added smile and worried faces to paulina

--- js/script.js
+++ js/script.js
@@ -80,8 +80,10 @@ const characters = {
 		"Name": "Paulina Martins",
 		"Color": "#f99dad",
 		"Images":{
-			"praia": "paulina/paulina-02.png",
-			// "clube": "paulina/paulina-03.png",
+			"praia_default": "paulina/paulina_praia_default.png",
+			"praia_smile": "paulina/paulina_praia_smile.png",
+			"praia_worried": "paulina/paulina_praia_worried.png",
+			"clube": "paulina/paulina_clube.png"
 		}
 	},
 	"char__mae": {
@@ -123,20 +125,23 @@ let script = {
 		"centered Cancun, Quintana Roo",
 		"scene BeachCabin with fadeIn",
 
-		"show char__paulina praia at left with fadeIn",
+		"show char__paulina praia_default at left with fadeIn",
+		"play sound cough",
 		"char__paulina Vê se não esquece de tomar o seu remédio na hora, mamãe.",
 		"char__mae Tá, filha, tá. Paulina. Vai depressa. Vai chegar atrasada ao trabalho. Não se preocupe comigo.",
-		"play sound cough",
 		"char__paulina Claro que eu me preocupo com você. Você é quem eu mais amo e é tudo que eu tenho.",
 		"char__mae Também tem o seu namorado, o Osvaldo, e com certeza gosta mais dele do que de mim.",
 
+		"show char__paulina praia_smile at left",
 		"char__paulina Não diga isso, mamãe. Não gosto mais do Osvaldo do que de você.",
 		"char__mae Paulina...",
 
+		"show char__paulina praia_default at left",
 		"char__paulina Eu sei, eu já sei, mamãe. Que o Osvaldo não é muito do seu agrado.",
 		"char__mae Não, filha. É verdade, não é. Mas se ele é o homem com quem pretende se casar, o que mais posso pedir?",
 		"char__mae Além de deixar você com alguém para quando eu morrer...",
 		"char__mae Só assim eu vou morrer tranquila, filha.",
+		"show char__paulina praia_worried at left",
 		"char__paulina Pára, mamãe. Não fala de morte porque eu não gosto. Você vai se curar.",
 		"jump on_the_way_to_club"
 	],
@@ -156,7 +161,7 @@ let script = {
 
 		"show char__osvaldo praia at right with fadeIn",
 		"char__osvaldo Paulina?",
-		"show char__paulina praia at left with fadeIn",
+		"show char__paulina praia_smile at left with fadeIn",
 		"char__paulina Osvaldo, meu amor!",
 
 		"char__osvaldo Aonde vai, com tanta pressa?",
@@ -165,14 +170,18 @@ let script = {
 		"char__paulina  Hoje eu entro mais cedo para trabalhar no clube porque a Margarida está doente.",
 
 		"char__osvaldo Então... só nos veremos amanhã?",
+		"show char__paulina praia_default at left",
 		"char__paulina Não tem jeito, meu amor. É o meu trabalho, e você sabe que eu preciso dele!",
 
 		"char__osvaldo Como está a Dona Paula?",
 		"char__paulina A cada dia pior, Osvaldo. E só sabe falar de morte...",
 
 		"char__osvaldo Tem que se acostumar com a idea de que ela vai morrer, Paulina.",
+		"show char__paulina praia_worried at left",
 		"char__paulina Eu não posso, Osvaldo. Não é fácil. É triste vê-la assim como está, destruída, quase... quase sem forças.",
+		"show char__paulina praia_default at left",
 		"char__paulina As vezes eu penso que o que a mantém viva é o medo de me deixar sozinha.",
+		"show char__paulina praia_worried at left",
 		"char__paulina Sabe o que ela disse? Que só poderá morrer tranquila no dia em que me ver casada com você.",
 
 		"play sound choice",
@@ -200,6 +209,7 @@ let script = {
 	        }
 	    },
 
+		"show char__paulina praia_default at left",
 		"char__paulina Eu gostaria que nos casássemos antes de... Chegar o momento.",
 		"char__paulina Podemos nos casar para dar esta alegria à minha mãe, Osvaldo? Diz pra mim que sim.",
 		"char__osvaldo Sim, sim, claro que sim, amor.",
@@ -226,7 +236,8 @@ let script = {
 	    },
 
 		"play sound busArriving",
-		"char__paulina ... Tenho que ir. Me leva até o clube?",
+		"show char__paulina praia_default at left",
+		"char__paulina Tenho que ir. Me leva até o clube?",
 		"char__osvaldo Não, não... Eu não posso. Eu tenho que fazer algumas coisas, mas eu apareço mais tarde no clube.",
 		"char__paulina Tá.",
 		// "char__osvaldo Ricardo. Ricardo! Me dá uma moeda para eu ligar para a capital.",
@@ -256,4 +267,7 @@ let script = {
 		"centered Your score: Osvaldo: {{player.respect_osvaldo}}",
 		"end"
 	]
+
+
+	// First "mission": act like Paola Bracho in the club, introducing the dressing min-game concept.
 };
